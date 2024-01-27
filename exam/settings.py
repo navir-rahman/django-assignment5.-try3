7a9a18603b3932cbaae22b16636723551222ca3a
navir-rahman <navirrahman@gmail.com>
2024-01-28 01:38:23 +0600

    portgresql added

--- exam/settings.py
+++ exam/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,17 +76,22 @@ WSGI_APPLICATION = 'exam.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library',
-        'USER': 'postgres',
-        "PASSWORD":'12345',
-        'Host': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://library_i708_user:K9NcEnTJYDZNMvWVRYbKiR6w0mWFftBI@dpg-cmqll1n109ks73fdgam0-a.oregon-postgres.render.com/library_i708',
+
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'library',
+#         'USER': 'postgres',
+#         "PASSWORD":'12345',
+#         'Host': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
